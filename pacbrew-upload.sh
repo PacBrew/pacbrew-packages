84586efd7918321b8ee5fcbc933642c1fae7bd9d
cpasjuste <cpasjuste@gmail.com>
2021-12-16 08:32:44 +0000

    pacbrew: update upload script

--- pacbrew-upload.sh
+++ pacbrew-upload.sh
@@ -3,12 +3,12 @@
 set -e
 
 b() {
-    scp $1/*.pkg.tar.xz cpasjuste@mydedibox.fr:/var/www/pacbrew/packages/
+    scp $1/*.pkg.tar.xz mydedibox.fr:/var/www/pacbrew/packages/
     pacbrew-repo-add pacbrew-repo/pacbrew.db.tar.gz $1/*.pkg.tar.xz
 }
 
 rm -rf pacbrew-repo
 mkdir -p pacbrew-repo
 . pacbrew-packages.sh
-scp pacbrew-repo/* cpasjuste@mydedibox.fr:/var/www/pacbrew/packages/
+scp pacbrew-repo/* mydedibox.fr:/var/www/pacbrew/packages/
 rm -rf pacbrew-repo
