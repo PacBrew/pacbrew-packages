6a9038b77fe4522a563f1c251f4d59602cb001d0
cpasjuste <cpasjuste@gmail.com>
2021-12-16 11:20:28 +0000

    pacbrew: allow single package build/upload, fix indentation

--- pacbrew.sh
+++ pacbrew.sh
@@ -3,40 +3,54 @@
 set -e
 
 function build {
-	b() {
-		pushd $1
-		pacbrew-makepkg -C -f
-		sudo pacbrew-pacman --noconfirm -U *.pkg.tar.xz
-		popd
-	}
+  b() {
+    pushd $1
+    pacbrew-makepkg -C -f
+    sudo pacbrew-pacman --noconfirm -U *.pkg.tar.xz
+    popd
+  }
 
-	# build packages
-	. pacbrew-packages.sh
+  if [ -z "$1" ]; then
+    # build all packages
+    . pacbrew-packages.sh
+  else
+    # build specified package
+    pushd $1
+    pacbrew-makepkg -C -f
+    sudo pacbrew-pacman --noconfirm -U *.pkg.tar.xz
+    popd
+  fi
 }
 
 function upload {
-	b() {
-		scp $1/*.pkg.tar.xz mydedibox.fr:/var/www/pacbrew/packages/
-		pacbrew-repo-add pacbrew-repo/pacbrew.db.tar.gz $1/*.pkg.tar.xz
-	}
+  b() {
+    scp $1/*.pkg.tar.xz mydedibox.fr:/var/www/pacbrew/packages/
+    pacbrew-repo-add pacbrew-repo/pacbrew.db.tar.gz $1/*.pkg.tar.xz
+  }
 	
-	# cleanup repo directory
-	rm -rf pacbrew-repo
-	mkdir -p pacbrew-repo
-	# get repo files from server
-	scp mydedibox.fr:/var/www/pacbrew/packages/pacbrew.* pacbrew-repo
-	# update and upload packages
-	. pacbrew-packages.sh
-	# upload updated repo files
-	scp pacbrew-repo/* mydedibox.fr:/var/www/pacbrew/packages/
-	# cleanup
-	rm -rf pacbrew-repo
+  # cleanup repo directory
+  rm -rf pacbrew-repo
+  mkdir -p pacbrew-repo
+  # get repo files from server
+  scp mydedibox.fr:/var/www/pacbrew/packages/pacbrew.* pacbrew-repo
+  if [ -z "$1" ]; then
+    # update and upload all packages
+    . pacbrew-packages.sh
+  else
+    # update and upload specified package
+    scp $1/*.pkg.tar.xz mydedibox.fr:/var/www/pacbrew/packages/
+    pacbrew-repo-add pacbrew-repo/pacbrew.db.tar.gz $1/*.pkg.tar.xz
+  fi
+  # upload updated repo files
+  scp pacbrew-repo/* mydedibox.fr:/var/www/pacbrew/packages/
+  # cleanup
+  rm -rf pacbrew-repo
 }
 
 if [ "$1" == "upload" ]; then
-	build
-	upload
+  build "$2"
+  upload "$2"
 else
-	build
+  build "$1"
 fi
 
