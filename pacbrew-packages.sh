76c502ea842b0aaa92b3ef5bc66f4f9c8eefbd82
cpasjuste <cpasjuste@gmail.com>
2021-12-20 14:33:38 +0100

    ps4: add ps4-orbisdev-glm

--- pacbrew-packages.sh
+++ pacbrew-packages.sh
@@ -7,7 +7,6 @@ b ps4-orbisdev/headers
 b ps4-orbisdev/libcxx
 b ps4-orbisdev/linker
 b ps4-orbisdev/libgen
-b ps4-orbisdev/libgen-9.00
 b ps4-orbisdev/vars
 b ps4-orbisdev/pkg-config
 b ps4-orbisdev/lief
@@ -27,3 +26,4 @@ b ps4-orbisdev-portlibs/libconfig
 b ps4-orbisdev-portlibs/libjson-c
 b ps4-orbisdev-portlibs/liblzma
 b ps4-orbisdev-portlibs/tinyxml2
+b ps4-orbisdev-portlibs/glm
